54d2b89d8b9a5e76dd8a5c6be6bfa24968d21dbf
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-04-05 18:55:01 +0530

    hgh

--- Google chrome/background.js
+++ Google chrome/background.js
@@ -1,53 +1,49 @@
-chrome.webRequest.onHeadersReceived.addListener
-  (
-    function (info) {
-      var headers = info.responseHeaders;
-      for (var i = headers.length - 1; i >= 0; --i) {
-        var header = headers[i].name.toLowerCase();
-        if (header == 'x-frame-options' || header == 'frame-options' || header == 'content-security-policy' || header == 'x-content-type-options' || header == 'x-xss-protection') {
-          headers.splice(i, 1); // Remove header
-        }
-      }
-      return { responseHeaders: headers };
-    },
-    {
-      urls: ['*://*/*'],
-      types: ['sub_frame',"sub_frame","ping"]
-    },
-    ['blocking', 'responseHeaders']
-  );
+chrome.runtime.onStartup.addListener(async () => {
+  var a = new Promise(function(resolve, reject){
+        chrome.storage.sync.get({"enabled": true}, function(options){
+            resolve(options.enabled);
+      })
+  });
+  console.log("enable");
+  const enabled = await a;
+  if (enabled) {
+    await enable();
+  } else {
+    await disable();
+  }
+});
 
-// Get user agent change from panel.js:
-var userAgent = "";
-chrome.runtime.onMessage.addListener
-  (
-    function (request, sender) {
-      if (request.userAgent) {
-        userAgent = request.userAgent;
-      }
+chrome.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "sync") return;
+
+  if (changes.enabled) {
+    if (changes.enabled.newValue) {
+      await enable();
+    } else {
+      await disable();
     }
-  );
+  }
+});
 
-// Set the right user agent when a page is loaded:
-chrome.webRequest.onBeforeSendHeaders.addListener
-  (
-    function (details) {
-      if (details.tabId === -1) {
-        var headers = details.requestHeaders;
+/**
+ * Enables this extension core
+ * @returns Promise
+ */
 
-        if (userAgent === "mobile") {
-          for (var i = 0; i < headers.length; i++) {
-            if (headers[i].name.toLowerCase() == 'user-agent') {
-              headers[i].value = "Mozilla/5.0 (Linux; Android 5.0; LG-D855 Build/LRX21R.A1445306351) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Mobile Safari/537.36 OPR/36.2.2126.102826";
-            }
-          }
-        }
+async function enable() {
+  await chrome.declarativeNetRequest.updateEnabledRulesets({
+    enableRulesetIds: ["youtube"],
+  });
+  console.log("enable");
+}
 
-        return { requestHeaders: headers };
-      }
-    },
-    {
-      urls: ["<all_urls>"]
-    },
-    ["blocking", "requestHeaders"]
-  );
+/**
+ * @returns Promise
+ */
+
+async function disable() {
+  await chrome.declarativeNetRequest.updateEnabledRulesets({
+    disableRulesetIds: ["youtube"],
+  });
+  console.log("disable");
+}
